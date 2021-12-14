4fedfe62c7dabadbe9cd4d7e4392bef14f1ea891
CertIanS <91575364+CertIanS@users.noreply.github.com>
2021-12-13 22:36:14 -0500

    Update SearchScreen.js

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,11 +1,47 @@
-import React from "react";
-import {View, Text, StyleSheet} from "react-native";
+import React, {useReducer} from "react";
+import {View, Text, StyleSheet, TextInput} from "react-native";
 import { colors } from '../styles';
+import { MovieList } from '../components/List/MovieList';
+import { tmdb, formatDate } from '../utilities';
 
 export const SearchScreen = () => {
+    let initialState = {
+	    searchTitle: '',
+	    movies: [],
+        error: null,
+	};
+	const reducer = (state, newState) => ({ ...state, ...newState });
+    const [state, setState] = useReducer(reducer, initialState);
+
+    const fetchMovie = async (movieTitle) => {
+        setState({searchTitle: movieTitle});
+        try {
+            let data = await tmdb.search.movies({
+                query: state.searchTitle,
+                page: 1
+            });
+            data.results.forEach((e) => {
+                e.poster_path= `https://www.themoviedb.org/t/p/w600_and_h900_bestv2${e.poster_path}`;
+                if(e.release_date !== '')
+                    e.release_date = formatDate(e.release_date);
+                else
+                    e.release_date = 'Date Unknown';
+                e.score = `${e.vote_average * 10}% (${e.vote_count})`;
+            });
+            setState({ movies: [...data.results]});
+        }
+        catch (error) {
+            setState({ error: error });
+        }
+    }
     return (
         <View>
-            <Text style={{ fontSize: 30, color: colors.foreground }}>Search Screen</Text>
+            <TextInput
+            style={{ height: 40, borderColor: 'gray', borderWidth: 1, color: colors.foreground }}
+            value={state.searchTitle}
+            onChangeText={(text) => fetchMovie(text)}
+            />
+            <MovieList movies={ state.movies } />
         </View>
     )
 }
